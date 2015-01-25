33a1afeb0164dbd6d2c939aeaed4a6af2b31282f
leafdj <dylangedig@gmail.com>
2015-01-24 18:27:37 -0800

    Moved Dialog, created a sample dialogdefs file.

---
+++ GGJ2015_Unity/Assets/Fields/colourTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomColourTrigger : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        float r = Random.value;
+        float g = Random.value;
+        float b = Random.value;
+        other.gameObject.renderer.material.color = new Color(r, g, b);
+    }
+}
