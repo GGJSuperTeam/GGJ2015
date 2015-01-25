fdd69f944b7a9741b0d7f97db6ef999e8abc43f6
Nick Pettyjohn <n.pettyjohn@gmail.com>
2015-01-24 21:28:08 -0800

    Added Diego's Graphics
    Oh yee

--- GGJ2015_Unity/Assets/ZSort.cs
+++ GGJ2015_Unity/Assets/ZSort.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class ZSort : MonoBehaviour {
 	
-	private Vector3 newPos = Vector3.zero;
-    private GameObject deer;
-
     void Start() {
         renderer.sortingOrder = (int)(10 * (-renderer.bounds.center.y + renderer.bounds.size.y / 2));
     }
